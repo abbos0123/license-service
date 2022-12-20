6f27aabada9e501b2026ae90e18a660a497af4a1
abbos0123 <abbosfayziboev@gmail.com>
2022-12-20 16:38:20 +0500

    first

---
+++ src/test/java/com/uniquecode/licensingservice/LicensingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquecode.licensingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
